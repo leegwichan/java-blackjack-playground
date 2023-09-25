7c7e52d049fe51dac4cc6e6141aa8e023fa686b0
DESKTOP-E5VRH4Q\cnddk <cnddkscndgus@naver.com>
2023-09-25 14:36:09 +0900

    feat (Dealer) : 딜러의 포인트가 16 초과할 때 카드를 추가 시 예외를 던진다

--- src/main/java/blackjack/domain/participants/Dealer.java
+++ src/main/java/blackjack/domain/participants/Dealer.java
@@ -6,6 +6,7 @@ import blackjack.domain.card.ParticipantCards;
 public final class Dealer extends Participant {
 
     private static final int DRAWABLE_UPPER_LIMIT_POINT = 16;
+    private static final String ADDING_CARD_EXCEPTION_FORMAT = "딜러는 포인트가 %d 초과 시 카드를 추가할 수 없습니다";
 
     public Dealer(ParticipantCards cards) {
         super(cards);
@@ -13,5 +14,18 @@ public final class Dealer extends Participant {
 
     public boolean isCardAddable() {
         return super.getPoint() <= DRAWABLE_UPPER_LIMIT_POINT;
+    }
+
+    @Override
+    public void add(Card card) {
+        validatePoints();
+        super.add(card);
+    }
+
+    private void validatePoints() {
+        if (!isCardAddable()) {
+            String message = String.format(ADDING_CARD_EXCEPTION_FORMAT, DRAWABLE_UPPER_LIMIT_POINT);
+            throw new IllegalStateException(message);
+        }
     }
 }
--- src/test/java/blackjack/domain/participants/DealerTest.java
+++ src/test/java/blackjack/domain/participants/DealerTest.java
@@ -1,7 +1,11 @@
 package blackjack.domain.participants;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import blackjack.domain.card.Card;
+import blackjack.domain.card.CardLetter;
+import blackjack.domain.card.CardShape;
 import blackjack.domain.card.MockParticipantCards;
 import blackjack.domain.card.ParticipantCards;
 import org.junit.jupiter.api.DisplayName;
@@ -35,5 +39,16 @@ class DealerTest {
 
             assertThat(actual).isTrue();
         }
+    }
+
+    @Test
+    @DisplayName("포인트가 16 초과인 경우 카드를 추가하면 예외를 던진다")
+    void addTest_whenPointOver16_throwException() {
+        ParticipantCards cards = new MockParticipantCards(17);
+        Dealer dealer = new Dealer(cards);
+
+        assertThatThrownBy(() -> dealer.add(new Card(CardShape.HEART, CardLetter.ACE)))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessage("딜러는 포인트가 16 초과 시 카드를 추가할 수 없습니다");
     }
 }
