7c97ed404e18ff304462f474a125279274c5f232
DESKTOP-E5VRH4Q\cnddk <cnddkscndgus@naver.com>
2023-09-25 14:21:27 +0900

    feat (Participant) : 참가자의 카드 추가, 포인트 조회, 버스트 여부 조회 기능 구현

---
+++ src/main/java/blackjack/domain/participants/Participant.java
@@ -0,0 +1,25 @@
+package blackjack.domain.participants;
+
+import blackjack.domain.card.Card;
+import blackjack.domain.card.ParticipantCards;
+
+abstract class Participant {
+
+    private final ParticipantCards cards;
+
+    Participant(ParticipantCards cards) {
+        this.cards = cards;
+    }
+
+    public void add(Card card) {
+        cards.add(card);
+    }
+
+    public final boolean isBust() {
+        return cards.isBust();
+    }
+
+    public final int getPoint() {
+        return cards.calculatePoint();
+    }
+}
