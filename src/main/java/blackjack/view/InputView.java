7cd041d10d6761413c76ac05620f4edd8d3c70ef
DESKTOP-E5VRH4Q\cnddk <cnddkscndgus@naver.com>
2023-09-27 13:10:07 +0900

    feat (InputView) : 카드를 한 장 더 추가할 지 요청하는 기능 구현
    - y 또는 n 만의 입력을 허용함
    - 이외 값을 입력받을 경우, 예외를 던짐

--- src/main/java/blackjack/view/InputView.java
+++ src/main/java/blackjack/view/InputView.java
@@ -6,12 +6,16 @@ import blackjack.view.printer.Printer;
 import blackjack.view.reader.Reader;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 
 public class InputView {
 
     private static final String PLAYER_NAMES_REQUEST = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)";
+    private static final String CARD_ADDING_REQUEST_FORMAT = "%n%s는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)%n";
+
     private static final String NAME_DELIMITER = ",";
+    private static final Map<String, Boolean> MESSAGE_TO_BOOLEAN = Map.of("y", true, "n", false);
 
     private final Reader reader;
     private final Printer printer;
@@ -31,5 +35,15 @@ public class InputView {
         return Arrays.stream(names.split(NAME_DELIMITER))
                 .map(String::trim)
                 .collect(toList());
+    }
+
+    public boolean isAddingCard(String playerName) {
+        printer.print(String.format(CARD_ADDING_REQUEST_FORMAT, playerName));
+        String message = reader.read();
+
+        if (!MESSAGE_TO_BOOLEAN.containsKey(message)) {
+            throw new IllegalArgumentException("허용된 문자만 입력할 수 있습니다");
+        }
+        return MESSAGE_TO_BOOLEAN.get(message);
     }
 }
--- src/test/java/blackjack/view/InputViewTest.java
+++ src/test/java/blackjack/view/InputViewTest.java
@@ -1,6 +1,7 @@
 package blackjack.view;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import blackjack.view.printer.MockPrinter;
 import blackjack.view.reader.MockReader;
@@ -8,9 +9,13 @@ import blackjack.view.reader.Reader;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import java.util.List;
 
 class InputViewTest {
+
+    private static final String NEW_LINE = System.getProperty("line.separator");
 
     @Nested
     @DisplayName("플레이어 이름 입력 테스트")
@@ -52,6 +57,53 @@ class InputViewTest {
             List<String> actual = inputView.inputPlayerNames();
 
             assertThat(actual).isEqualTo(expected);
+        }
+    }
+
+    @Nested
+    @DisplayName("카드 추가 여부 입력 테스트")
+    class AddingCardTest {
+
+        private static final String INPUT_YES = "y";
+        private static final String INPUT_NO = "n";
+
+        @Test
+        @DisplayName("카드 추가 여부에 대한 입력 요청 메세지를 출력할 수 있다")
+        void isAddingCardTest_assertThatPrintRequestMessage() {
+            MockPrinter printer = new MockPrinter();
+            InputView inputView = new InputView(new MockReader(INPUT_YES), printer);
+            String name = "playerName";
+            String expectedPrintedMessage = NEW_LINE + name + "는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)" + NEW_LINE;
+
+            inputView.isAddingCard(name);
+
+            assertThat(printer.getPrintedMessage()).isEqualTo(expectedPrintedMessage);
+        }
+
+        @ParameterizedTest(name = "{0}으로 입력하면 {1}을 반환한다")
+        @CsvSource({INPUT_YES + ",true", INPUT_NO + ",false"})
+        @DisplayName("정해진 입력 메세지에 따라 카드 추가 여부를 판단한다")
+        void isAddingCardTest_assertThatJudgeByMessage(String inputMessage, boolean expected) {
+            Reader reader = new MockReader(inputMessage);
+            InputView inputView = new InputView(reader, new MockPrinter());
+            String name = "playerName";
+
+            boolean actual = inputView.isAddingCard(name);
+
+            assertThat(actual).isEqualTo(expected);
+        }
+
+        @ParameterizedTest
+        @CsvSource({"Y", "N", "yes", "x"})
+        @DisplayName("입력한 문자가 허용된 문자가 아닌 경우, 예외를 던진다")
+        void isAddingCardTest_whenInputAnotherMessage_throwException(String inputMessage) {
+            Reader reader = new MockReader(inputMessage);
+            InputView inputView = new InputView(reader, new MockPrinter());
+            String name = "playerName";
+
+            assertThatThrownBy(() -> inputView.isAddingCard(name))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage("허용된 문자만 입력할 수 있습니다");
         }
     }
 }
