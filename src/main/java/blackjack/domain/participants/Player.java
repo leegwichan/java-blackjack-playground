7530ab0fb1d9ed468633359eeb40e3e2dba6748c
DESKTOP-E5VRH4Q\cnddk <cnddkscndgus@naver.com>
2023-09-30 18:14:14 +0900

    feat (Player) : 베팅 금액을 입력 받고 반환하는 기능 구현

--- src/main/java/blackjack/domain/participants/Player.java
+++ src/main/java/blackjack/domain/participants/Player.java
@@ -1,17 +1,32 @@
 package blackjack.domain.participants;
 
 import blackjack.domain.card.ParticipantCards;
+import java.util.Objects;
 
 public final class Player extends Participant {
 
     private final String name;
+    private final int betAmount;
 
-    public Player(String name, ParticipantCards cards) {
+    public Player(String name, int betAmount, ParticipantCards cards) {
         super(cards);
-        this.name = name;
+        this.name = Objects.requireNonNull(name);
+
+        validateBetAmount(betAmount);
+        this.betAmount = betAmount;
+    }
+
+    private void validateBetAmount(int betAmount) {
+        if (betAmount < 0) {
+            throw new IllegalArgumentException("베팅 금액은 양수이어야 합니다");
+        }
     }
 
     public String getName() {
         return name;
+    }
+
+    public int getBetAmount() {
+        return betAmount;
     }
 }
