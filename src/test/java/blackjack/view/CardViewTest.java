340625dbbbcdc7a158613c9298cb3b566a6852ca
DESKTOP-E5VRH4Q\cnddk <cnddkscndgus@naver.com>
2023-10-01 13:45:44 +0900

    refactor (CardViewTest) : 각 테스트마다 객체 생성을 공통으로 해서 @BeforeEach로 분리함

--- src/test/java/blackjack/view/CardViewTest.java
+++ src/test/java/blackjack/view/CardViewTest.java
@@ -6,6 +6,7 @@ import blackjack.domain.card.CardLetter;
 import blackjack.domain.card.CardShape;
 import blackjack.dto.card.CardDto;
 import blackjack.dto.card.CardsDto;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -14,12 +15,13 @@ import java.util.List;
 
 class CardViewTest {
 
+    private CardView cardView;
+
     @ParameterizedTest
     @CsvSource({"DIAMOND,ACE,A다이아몬드", "CLOVER,EIGHT,8클로버", "SPADE,QUEEN,Q스페이드"})
     @DisplayName("카드 1장을 출력 형식으로 변환할 수 있다")
     void toCardsViewTest_whenCardsSizeIs1(CardShape shape, CardLetter letter, String expected) {
         CardsDto cards = CardsDto.of(List.of(CardDto.of(shape, letter)));
-        CardView cardView = new CardView();
 
         String actual = cardView.toCardsView(cards);
 
@@ -32,7 +34,6 @@ class CardViewTest {
         CardDto card1 = CardDto.of(CardShape.SPADE, CardLetter.KING);
         CardDto card2 = CardDto.of(CardShape.HEART, CardLetter.SIX);
         CardsDto cards = CardsDto.of(List.of(card1, card2));
-        CardView cardView = new CardView();
         String expected = "K스페이드, 6하트";
 
         String actual = cardView.toCardsView(cards);
@@ -47,7 +48,6 @@ class CardViewTest {
         CardDto card2 = CardDto.of(CardShape.HEART, CardLetter.SIX);
         CardDto card3 = CardDto.of(CardShape.HEART, CardLetter.NINE);
         CardsDto cards = CardsDto.of(List.of(card1, card2, card3));
-        CardView cardView = new CardView();
         String expected = "K스페이드, 6하트, 9하트";
 
         String actual = cardView.toCardsView(cards);
@@ -61,12 +61,16 @@ class CardViewTest {
         CardDto card1 = CardDto.of(CardShape.SPADE, CardLetter.KING);
         CardDto card2 = CardDto.of(CardShape.HEART, CardLetter.SIX);
         CardsDto cards = CardsDto.of(List.of(card1, card2));
-        CardView cardView = new CardView();
         String expected = "K스페이드";
 
         String actual = cardView.toFirstCardView(cards);
 
         assertThat(actual).isEqualTo(expected);
+    }
+
+    @BeforeEach
+    void initializeCardView() {
+        cardView = new CardView();
     }
 }
 
